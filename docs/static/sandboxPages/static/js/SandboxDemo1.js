2754f206d4f0d2323d08cb9d11e20967704eaf3a
zougt <zgtusers@outlook.com>
2021-10-18 17:50:56 +0800

    v0.6.1

--- docs/static/sandboxPages/static/js/SandboxDemo1.js
+++ docs/static/sandboxPages/static/js/SandboxDemo1.js
@@ -77,7 +77,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/anys/static/sandboxPages/";
+/******/ 	__webpack_require__.p = "/static/sandboxPages/";
 /******/
 /******/
 /******/ 	// Load entry module and return exports
